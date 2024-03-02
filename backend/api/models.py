5b0c453b75ad6babca9596fcd8f51c84f8c28d8b
axolp <j.w.ploska@gmail.com>
2024-03-02 15:31:08 +0100

    Odpowiadanie na flashcards, anki, dodałem tabele fiszka

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Fiszka(models.Model):
+    # Tutaj dodajesz pola dla swojego modelu, na przykład:
+    pytanie = models.CharField(max_length=255)
+    odpowiedz = models.CharField(max_length=255)
+    # Dodaj więcej pól zgodnie z potrzebami
+
+    def __str__(self):
+        return self.pytanie  # lub inna reprezentacja Twojego wyboru
 class UserProgress(models.Model):
     up_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
